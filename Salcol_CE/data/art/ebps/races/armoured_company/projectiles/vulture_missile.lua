9779a650e6b9af64029a43e79905c92aaa8464fe
79608 <mestkovskyyaroslavd@mail.ru>
2022-08-07 22:01:49 +0300

    commit #8 - часть файлов №8

---
+++ Salcol_CE/data/art/ebps/races/armoured_company/projectiles/vulture_missile.lua
@@ -0,0 +1,8 @@
+simvis_attributes =
+{
+	selection_volume		= 1,
+	selection_ground		= 1,
+
+	vis_scale_min			= 2.5,	
+	vis_scale_max			= 2.5,
+}
---
+++ Salcol_CE/data/art/ebps/races/chaos/troops/obliterator_kekoulis.lua
@@ -0,0 +1,27 @@
+simvis_attributes =
+{
+	melee_size 					= 0,
+	melee_block_ratio			= 50,
+	melee_unsynced_damage_ratio = 30,
+
+	selection_volume		= 1,
+	selection_ground		= 1,
+
+        vis_scale_min = 1.3,
+        vis_scale_max = 1.35,
+
+	-- Shadow Types:
+	-- 0 = None
+	-- 1 = blob (rotated by light dir)
+	-- 2 = blob (rotated by object dir)
+	-- 3 = shadow map
+	-- 4 = stencil shadow volume
+	shadow_type			= 3,
+	shadow_scale		= 0.0275,
+	
+	infantry_physics	    		= 1,
+	infantry_sway_update_time		= 4,
+	infantry_sway_random_radius		= 0.4,
+	
+		occludee	= 1,
+}
